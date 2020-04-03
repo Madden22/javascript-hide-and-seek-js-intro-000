60377b4abebeb7cabcccbc1d3e2dce66cf67f1bb
Sydney Michael <m13s19madden@gmail.com>
2020-04-03 23:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const all = document.querySelectorAll(ul.ranked-list)
   for (let i = 0; i < all.length; i++) {
-    all[i] += n
+    all[i].innerHTML += n
   }
   return all
 }
