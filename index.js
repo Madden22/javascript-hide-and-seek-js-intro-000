79269026dcacc3d4a492110aedc3fac22460454f
Sydney Michael <m13s19madden@gmail.com>
2020-04-03 23:06:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(sel) {
 }
 
 function nestedTarget() {
-  return document.querySelector('div.nested').getElementsByClassName('target')
+  return document.querySelector('div#nested div.target')
 }
 
 function increaseRankBy() {
